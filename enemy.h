f86546a591e76cee53c6092ccf963cbf884d55d4
David Wang <brwangdavid1027@gmail.com>
2023-04-03 16:44:21 -0400

    modified:   enemy.h 	modified:   phoenix.h

--- enemy.h
+++ enemy.h
@@ -3,12 +3,12 @@
 
 #include "entity.h"
 
-class Enemy : public Entity {
-    public:
-        void attack(Entity& whodefend) override;
-        virtual void dlcAttack(Entity& whodefend) = 0;
-        virtual void move() override;
-        virtual void notify(Subject<State> $whoNotified);
+class Enemy : public Entity{
+public:
+    void attack(Entity& whodefend) override;
+    virtual void dlcAttack(Entity& whodefend) = 0;
+    virtual void move() override;
+    virtual void notify(Subject<State> $whoNotified);
 };
 
 #endif
