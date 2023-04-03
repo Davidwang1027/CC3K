f03c1137c560dd16e6097614958233c6f7225d0e
David Wang <brwangdavid1027@gmail.com>
2023-04-03 15:19:18 -0400

    modified:   textdisplay.cc

--- textdisplay.cc
+++ textdisplay.cc
@@ -15,6 +15,9 @@ void TextDisplay::notify(Subject<State>& whoNotified){
     case CellType::hWall:
         theDisplay[r][c] = '-';
         break;
+    case CellType::passage:
+        theDisplay[r][c] = '#';
+        break;
     case CellType::door:
         theDisplay[r][c] = '+';
         break;
@@ -40,24 +43,56 @@ void TextDisplay::notify(Subject<State>& whoNotified){
     case CellType::compass:
         theDisplay[r][c] = 'C';
         break;
-
-
-
-
-
-
+    case CellType::suit:
+        theDisplay[r][c] = 'S';
+        break;
+    case CellType::player:
+        theDisplay[r][c] = '@';
+        break;
+    case CellType::vampire:
+        theDisplay[r][c] = 'V';
+        break;
+    case CellType::troll:
+        theDisplay[r][c] = 'T';
+        break;
+    case CellType::goblin:
+        theDisplay[r][c] = 'N';
+        break;
+    case CellType::dragon:
+        theDisplay[r][c] = 'D';
+        break;
+    case CellType::merchant:
+        theDisplay[r][c] = 'M';
+        break;
+    case CellType::phoenix:
+        theDisplay[r][c] = 'X';
+        break;
     }
 }
 
 std::ostream& operator<<(std::ostream& out, const TextDisplay& td){
     for (auto row : td.theDisplay){
         for (auto col : row){
-            if (col == '@'){
-                out << ' ';
-            } else
+            switch (col){
+            case '@':
+                out << "\033[31m@\033[0m";
+                break;
+            case 'V':
+                out << "\033[31;2mV\033[0m";
+                break;
+            case 'N':
+                out << "\033[32mG\033[0m";
+                break;
+            case '\\':
+                out << "\033[31m@\033[0m";
+                break;
+            case 'G':
+                out << "\033[38;5;214mG\033[0m";
+            default:
                 out << col;
+            }
+            out << std::endl;
         }
-        out << std::endl;
+        return out;
     }
-    return out;
 }
