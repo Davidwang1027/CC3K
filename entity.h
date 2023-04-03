851b15741317447f5c9052e7913bb5881da9cf8e
Haoru Zhang <harryzhang20030502@gmail.com>
2023-04-02 20:32:41 -0400

    modified:   entity.h

--- entity.h
+++ entity.h
@@ -10,12 +10,12 @@ public:
     Entity(int hp, int atk, int def): atk{atk}, hp{hp}, def{def} {}
     virtual void attack() = 0;
     virtual void move() = 0;
-    int getHp() { return hp; };
-    int getAtk() { return atk; };
-    int getDef() { return def; };
-    void setHp(int new_hp) { hp = new_hp; };
-    void setAtk(int new_atk) { atk = new_atk; };
-    void setDef(int new_def) { def = new_def; };
+    virtual int getHp() { return hp; };
+    virtual int getAtk() { return atk; };
+    virtual int getDef() { return def; };
+    virtual void setHp(int new_hp) { hp = new_hp; };
+    virtual void setAtk(int new_atk) { atk = new_atk; };
+    virtual void setDef(int new_def) { def = new_def; };
 };
 
 #endif
