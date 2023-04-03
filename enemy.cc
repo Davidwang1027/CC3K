9fe3ea8273838c8f6a6200cf2096e7779919f3f8
Haoru Zhang <harryzhang20030502@gmail.com>
2023-04-03 17:29:54 -0400

    modified:   enemy.cc 	modified:   subject.h

--- enemy.cc
+++ enemy.cc
@@ -27,10 +27,14 @@ bool miss() {
 }
 
 void Enemy::attack(Entity& whodefend){
-    int damage = ceiling((100 / (100 + whodefend.getDef())) * this->getAtk());
-    whodefend.setHp(this->getHp() - damage);
+    if (!miss()) {
+        int damage = ceiling((100 / (100 + whodefend.getDef())) * this->getAtk());
+        whodefend.setHp(this->getHp() - damage);
+    } 
+    
 }
 
 void Enemy::move() {
     //Need random generation
 }
+
