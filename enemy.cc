85511dd9faf7eb5e155ed67de36a5e57d5c8e402
David Wang <brwangdavid1027@gmail.com>
2023-04-03 15:50:57 -0400

    modified:   enemy.cc 	modified:   enemy.h 	new file:   phoenix.h

--- enemy.cc
+++ enemy.cc
@@ -1,15 +1,15 @@
 #include "enemy.h"
 
-int ceiling(int num) {
-    if (num % 2 == 0) {
+int ceiling(int num){
+    if (num % 2 == 0){
         return num;
-    } else {
+    } else{
         return num + 1;
     }
 }
 
-void Enemy::attack(Entity& whodefend) {
-    int damage = ceiling((100/(100 + whodefend.getDef())) * this->getAtk());
+void Enemy::attack(Entity& whodefend){
+    int damage = ceiling((100 / (100 + whodefend.getDef())) * this->getAtk());
     whodefend.setHp(this->getHp() - damage);
 }
 
--- enemy.h
+++ enemy.h
@@ -3,11 +3,11 @@
 
 #include "entity.h"
 
-class Enemy : public Entity {
-    public:
-        virtual void attack(Entity& whodefend) override;
-        virtual void dlcAttack(Entity& whodefend) = 0;
-        virtual void move() override;
+class Enemy : public Entity{
+public:
+    virtual void attack(Entity& whodefend) override;
+    virtual void dlcAttack(Entity& whodefend) = 0;
+    virtual void move() override;
 };
 
 #endif
