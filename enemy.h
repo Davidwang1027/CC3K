329a8fadc90facb1d5947a86fa1bde38222b453e
Haoru Zhang <harryzhang20030502@gmail.com>
2023-04-03 15:50:17 -0400

    modified:   enemy.h

--- enemy.h
+++ enemy.h
@@ -6,6 +6,7 @@
 class Enemy : public Entity {
     public:
         virtual void attack(Entity& whodefend) override;
+        virtual void dlcAttack(Entity& whodefend) = 0;
         virtual void move() override;
 };
 
