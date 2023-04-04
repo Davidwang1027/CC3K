0a33d0d98466230f701932a3025abfd7fcee5797
Haoru Zhang <harryzhang20030502@gmail.com>
2023-04-04 17:50:20 -0400

    modified:   wd.h

--- wd.h
+++ wd.h
@@ -8,6 +8,7 @@ class WD : public Tempotion {
     public:
         WD(Player *component) : Tempotion{component} {}
         int getDef() override { return component->getDef() - 5; }
+        void use(Player* p) override;
 };
 
 #endif
