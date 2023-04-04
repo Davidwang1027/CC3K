bbc81ea4e2e76b0578338fcbea517d30cafca2ce
Tianjian Hao <haotianjian_lucky9@163.com>
2023-04-04 17:54:27 -0400

    modified:   gold.h 	modified:   suit.h

--- suit.h
+++ suit.h
@@ -1,8 +1,25 @@
+#ifndef SUIT_H
+#define SUIT_H
 #include "item.h"
 #include "decorator.h"
+#include "Player.h"
+#include "dragon.h"
+
 
 class Player;
 class Suit : public Item, public Decorator{
-    public:
-        Suit(Player *component);
+    bool isProtected = false;
+    Dragon* dargon = nullptr;
+public:
+    Suit(Player *component) : Decorator(component) {}
+    void setIsProtected(bool isProtected) {
+        this->isProtected = isProtected;
+    }
+    void use(Player*& p) {
+
+    }
+    
+
 };
+
+#endif
