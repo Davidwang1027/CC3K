fa4d81af617016def2f488caf1bff1ed736fd7b0
David Wang <brwangdavid1027@gmail.com>
2023-04-04 18:24:52 -0400

    modified:   player.h

--- player.h
+++ player.h
@@ -5,15 +5,16 @@
 class Player : public Entity{
 private:
     int maxhp;
-    bool Suit;
+    bool hasSuit;
 protected:
     float gold;
 public:
-    Player(int maxhp = 0, int hp = 0, int atk = 0, int def = 0, float gold = 0, bool Suit = false) : Entity{ hp, atk, def }, maxhp{ maxhp }, gold{ gold }, Suit{Suit} {}
+    Player(int maxhp = 0, int hp = 0, int atk = 0, int def = 0, float gold = 0, bool Suit = false) : Entity{ hp, atk, def }, maxhp{ maxhp }, gold{ gold }, Suit{ Suit }{}
     virtual void attack(Enemy& whodefend);
     virtual void addGold(float plusgold);
     float getGold();
-    bool hasSuit() {return Suit;}
+    bool getSuit(){ return hasSuit; }
+    void setSuit(bool suit){ hasSuit = suit; }
 };
 
 #endif
