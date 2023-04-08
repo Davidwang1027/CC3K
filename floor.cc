b5601d2dc0e0577bc5d773aa56eb440c1ede13e5
Haoru Zhang <harryzhang20030502@gmail.com>
2023-04-08 16:14:18 -0400

    modified:   floor.cc

--- floor.cc
+++ floor.cc
@@ -412,10 +412,39 @@ std::string Floor::navigation(Position dir){
     for (int i = -1; i < 2; i++){
         for (int j = -1; j < 2; j++){
             if (theFloor.at(dir.x+i).at(dir.y+j).getCellType() == CellType::goblin) {
-                theFloor.at(dir.x+i).at(dir.y+j).setState({ {dir.x+i, dir.y+j}, CellType::goblin})
-            }
+                result += "and sees a Goblin.";
+            } else if (theFloor.at(dir.x+i).at(dir.y+j).getCellType() == CellType::vampire) {
+                result += "and sees a Vampire.";
+            } else if (theFloor.at(dir.x+i).at(dir.y+j).getCellType() == CellType::troll) {
+                result += "and sees a Troll.";
+            } else if (theFloor.at(dir.x+i).at(dir.y+j).getCellType() == CellType::dragon) {
+                result += "and sees a Dragon.";
+            } else if (theFloor.at(dir.x+i).at(dir.y+j).getCellType() == CellType::werewolf) {
+                result += "and sees a Werewolf.";
+            } else if (theFloor.at(dir.x+i).at(dir.y+j).getCellType() == CellType::merchant) {
+                result += "and sees a Merchant.";
+            } else if (theFloor.at(dir.x+i).at(dir.y+j).getCellType() == CellType::phoenix) {
+                result += "and sees a Phoenix.";
+            } else if (theFloor.at(dir.x+i).at(dir.y+j).getCellType() == CellType::potion) {
+                result += "and sees an unknown potion.";
+            } else if (theFloor.at(dir.x+i).at(dir.y+j).getCellType() == CellType::gold) {
+                if ((theFloor.at(dir.x+i).at(dir.y+j).getGold()->getIsProtected())) {
+                    result += "and sees a dragon hoard.";
+                } else {
+                    result += "and sees a pile of golds.";
+                }
+            } else if (theFloor.at(dir.x+i).at(dir.y+j).getCellType() == CellType::suit) {
+                if ((theFloor.at(dir.x+i).at(dir.y+j).getSuit()->getIsProtected())) {
+                    result += "and sees a Barrier Suit protected by a dragon.";
+                } else {
+                    result += "and sees a Barrier Suit.";
+                }
+            } else if (theFloor.at(dir.x+i).at(dir.y+j).getCellType() == CellType::compass) {
+                result += "and sees a Compass.";
+            } 
         }
     }
+    return result;
 }
 
 void Floor::playerMove(Position dir){
@@ -485,10 +514,4 @@ bool Floor::isOnStair(){
 
 Floor::~Floor(){
     delete td;
-    for (auto e : enemies){
-        delete e;
-    }
-    for (auto i : items){
-        delete i;
-    }
 }
